bfdd569092a9f87be0e9759254d5699eddc1b418
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-04-17 14:01:34 -0400

    jkjkl

--- poopy/src/views/HomeView.vue
+++ poopy/src/views/HomeView.vue
@@ -7,5 +7,10 @@ let sex = ref('')
 let deaths = ref('')
 </script>
 <template>
-  <main></main>
+  <label>year:</label>
+  <select>
+    <option>2019</option>
+    <option>2018</option>
+    <option>2017</option>
+  </select>
 </template>
