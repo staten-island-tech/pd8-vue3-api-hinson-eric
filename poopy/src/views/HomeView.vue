deebeb7a57f11cc81ac71c65a442919d462a38fb
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-04-17 13:59:03 -0400

    afsdfasd

--- poopy/src/views/HomeView.vue
+++ poopy/src/views/HomeView.vue
@@ -1,5 +1,11 @@
-<script setup></script>
-
+<script setup>
+import { ref } from 'vue'
+let year = ref('')
+let race = ref('')
+let cause = ref('')
+let sex = ref('')
+let deaths = ref('')
+</script>
 <template>
   <main></main>
 </template>
