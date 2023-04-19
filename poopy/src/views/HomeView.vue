04b2debe8cade1a9de432d0e6a19a300001d4fd2
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-04-19 14:01:39 -0400

    asdfas

--- poopy/src/views/HomeView.vue
+++ poopy/src/views/HomeView.vue
@@ -5,12 +5,52 @@ let race = ref('')
 let cause = ref('')
 let sex = ref('')
 let deaths = ref('')
+let currentArray = ref([])
+let funnyApiTemp = ref(`https://data.cityofnewyork.us/resource/jb7j-dtam.json`)
+let funnyApiTemp2 = await fetch(funnyApiTemp)
+let funnyApi = await funnyApiTemp2.json()
+
+function filterArray() {
+  console.log(funnyApi)
+}
+filterArray()
 </script>
 <template>
-  <label>year:</label>
-  <select>
-    <option>2019</option>
-    <option>2018</option>
-    <option>2017</option>
-  </select>
+  <div id="duck">
+    <h2>filter graph by</h2>
+    <p>select year:</p>
+    <select v-model="year">
+      <option>all years</option>
+      <option>2019</option>
+      <option>2018</option>
+      <option>2017</option>
+      <option>2016</option>
+      <option>2015</option>
+      <option>2014</option>
+      <option>2013</option>
+      <option>2012</option>
+    </select>
+    <p>select sex:</p>
+    <select v-model="sex">
+      <option>both</option>
+      <option>M</option>
+      <option>F</option>
+    </select>
+    <p>select race/ethnicity</p>
+    <select v-model="race">
+      <option>asian</option>
+      <option>hispanic</option>
+      <option>non hispanic white</option>
+      <option>black</option>
+      <option>other</option>
+    </select>
+    <p>your year is {{ year }}, sex is {{ gender }}, races is {{ races }}</p>
+  </div>
 </template>
+
+<style>
+#duck {
+  display: flex;
+  flex-direction: column;
+}
+</style>
